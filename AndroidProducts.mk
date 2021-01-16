95f4644038452a676ac323c0ecb2aa7e48e65430
Axaroid <aksharndrds@gmail.com>
2021-01-16 15:56:19 +0530

    lineage --> evolution

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_santoni.mk
+    $(LOCAL_DIR)/evolution_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_santoni-userdebug \
-    lineage_santoni-user \
-    lineage_santoni-eng
+    evolution_santoni-userdebug \
+    evolution_santoni-user \
+    evolution_santoni-eng
